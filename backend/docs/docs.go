2e06805679cd26e6f2a38762f211e6ef0d9f7f6b
MacBook <macbook@MacBook-Air-MacBook.local>
2026-06-30 20:17:09 +0300

    docs: regenerate swagger for quota fields

--- backend/docs/docs.go
+++ backend/docs/docs.go
@@ -2865,6 +2865,18 @@ const docTemplate = `{
                 "second_name": {
                     "type": "string"
                 },
+                "share_links_count": {
+                    "type": "integer"
+                },
+                "share_links_quota": {
+                    "type": "integer"
+                },
+                "shared_dirs_count": {
+                    "type": "integer"
+                },
+                "shared_dirs_quota": {
+                    "type": "integer"
+                },
                 "storage_quota": {
                     "type": "integer"
                 },
@@ -3629,6 +3641,12 @@ const docTemplate = `{
                 },
                 "second_name": {
                     "type": "string"
+                },
+                "share_links_count": {
+                    "type": "integer"
+                },
+                "share_links_quota": {
+                    "type": "integer"
                 },
                 "shared_dirs_count": {
                     "type": "integer"
